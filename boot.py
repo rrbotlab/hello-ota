c3f0821415292b658e54f799231b65235f9f01df
rrbotlab <4213391+rrbotlab@users.noreply.github.com>
2025-02-13 09:40:50 -0300

    hash tests

--- boot.py
+++ boot.py
@@ -1,3 +1,4 @@
+# boot ota
 # This file is executed on every boot (including wake-boot from deepsleep)
 # import esp
 # esp.osdebug(None)
@@ -9,6 +10,7 @@ import gc
 # import mrequests as requests
 import urequests
 import uhashlib
+from ubinascii import hexlify
 
 gc.collect()
 
@@ -17,10 +19,17 @@ gc.collect()
 def boot():
     try:
         gc.collect()
+        hash_local = hexlify(uhashlib.sha256(open('hello_ota.py').read()).digest())
         r = urequests.get("https://raw.githubusercontent.com/rrbotlab/hello-ota/refs/heads/main/hello_ota.py") #?status=up&msg=OK&ping=1")
         # r = urequests.get("https://www.google.com") #?status=up&msg=OK&ping=1")
-        print('r.response: ', r.status_code)
+        print('r.response: \t', r.status_code)
+        hash_remote = hexlify(uhashlib.sha256(r.content).digest())
+        print('hash_local\t', hash_local)
+        print('hash_remote\t', hash_remote)
+        # with open('temp', "a") as fp:
+        #     fp.write(r.content)
         r.close()
+        gc.collect()
 
     except Exception as ex:
         print("boot error:", str(ex))
