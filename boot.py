cff31ec956564bd1e8364378bdd6ed61263c3316
rrbotlab <4213391+rrbotlab@users.noreply.github.com>
2025-02-12 22:13:58 -0300

    scratch

--- boot.py
+++ boot.py
@@ -6,5 +6,25 @@ import uos, machine
 import gc
 # import webrepl
 # webrepl.start()
+# import mrequests as requests
+import urequests
+import uhashlib
+
 gc.collect()
 
+# https://raw.githubusercontent.com/rrbotlab/hello-ota/refs/heads/main/hello_ota.py
+
+def boot():
+    try:
+        gc.collect()
+        r = urequests.get("https://raw.githubusercontent.com/rrbotlab/hello-ota/refs/heads/main/hello_ota.py") #?status=up&msg=OK&ping=1")
+        # r = urequests.get("https://www.google.com") #?status=up&msg=OK&ping=1")
+        print('r.response: ', r.status_code)
+        r.close()
+
+    except Exception as ex:
+        print("boot error:", str(ex))
+
+
+if __name__ == '__main__':
+    boot()
---
+++ hello_ota.py
@@ -0,0 +1,1 @@
+print('main -> hello_ota.py')
